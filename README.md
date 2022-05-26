5dfb5e228ad33410129cc7dbd2bf17a23ce40398
wendycorzo <wendycorzo@colegioguanenta.edu.co>
2022-05-26 18:20:18 -0500

    Desarrollo taller Potencia

---
+++ README.md
@@ -0,0 +1,5 @@
+# progama No.2
+
+### Progama para calcular X^Y
+
+![Diagrama de flujo](diagrama.png "Diagrama de flujo")
---
+++ potencia.py
@@ -0,0 +1,18 @@
+"""
+Problema No.2: Potencia X a la Y"""
+
+print("-----------------------------")
+print("-------Potencia X a la Y-----")
+print("-----------------------------")
+
+# Input
+X= input("digite el valor de X:")
+X= int(X)
+Y= input("digite el valor de Y:")
+Y= int(Y)
+
+#processing
+Z = X ** Y
+
+#output
+print( str (X) + " elevado a " + str (Y) + " es igual a: " + str (Z))
